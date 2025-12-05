544f205486aa76a4f3ec9324014ace875595ac47
AihoEs <efoefim@gmail.com>
2025-12-05 16:09:33 +0200

    XUnit tests

---
+++ Tests/MovieSearchTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using MoviesOnline;
+using MoviesOnline.Interfaces;
+using MoviesOnline.Services.HttpServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class MovieSearchTest
+    {
+        [Fact]
+        public async Task MovieSearchMock_Should_Get200()
+        {
+
+            var mockMovieApi = new Mock<IMovieAPICollector>();
+
+            var FakeResponse = new OmdbSearchResponse
+            {
+                Search = new List<OmdbMovie>
+                {
+                    new OmdbMovie { Title = "Inception", Year = "2010", imdbID = "tt1375666" }
+                }
+            };
+
+            mockMovieApi
+                .Setup(x => x.SearchMovieAsync("Inception"))
+                .ReturnsAsync(FakeResponse.Search);
+
+            var result = await mockMovieApi.Object.SearchMovieAsync("Inception");
+
+            Assert.NotNull(result);
+            Assert.Equal("Inception", result[0].Title);
+
+
+
+        }
+    }
+}
